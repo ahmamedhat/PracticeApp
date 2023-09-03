2d6c0db1cf1df1119b1dfec2b19249010d39d276
Medhat <ahmadmed7at77@gmail.com>
2023-09-03 16:33:09 +0300

    feat: initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View className="flex-1 justify-center items-center bg-red-300">
+      <Text className='text-2xl font-bold'>heeey</Text>
+    </View>
+  );
+}
+
+export default App;
