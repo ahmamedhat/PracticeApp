a662325509664277f86f1869db33ddd74480894c
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-09-16 14:23:28 +0300

    feat: add background image to login screen

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,36 @@
 module.exports = {
-	presets: ["module:metro-react-native-babel-preset"],
-	plugins: [
-		["module:react-native-dotenv", {
-			"envName": "APP_ENV",
-			"moduleName": "@env",
-			"path": ".env",
-			"blocklist": null,
-			"allowlist": null,
-			"safe": false,
-			"allowUndefined": true,
-			"verbose": false
-		}],
-		"nativewind/babel", [
-			"module-resolver",
-			{
-				root: ["/"],
-				extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
-				alias: {
-					"@screens": "./src/screens/index",
-					"@components": "./src/components/index",
-					"@navigation": "./src/navigation/index",
-					"@redux": "./src/redux",
-					"@utils": "./src/utils",
-					"@icons": "./src/assets/icons/index",
-				},
-			},
-		], "react-native-reanimated/plugin"],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module:react-native-dotenv",
+      {
+        envName: "APP_ENV",
+        moduleName: "@env",
+        path: ".env",
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+    "nativewind/babel",
+    [
+      "module-resolver",
+      {
+        root: ["/"],
+        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+        alias: {
+          "@screens": "./src/screens/index",
+          "@components": "./src/components/index",
+          "@navigation": "./src/navigation/index",
+          "@redux": "./src/redux",
+          "@utils": "./src/utils",
+          "@icons": "./src/assets/icons/index",
+          "@images": "./src/assets/images/index",
+        },
+      },
+    ],
+    "react-native-reanimated/plugin",
+  ],
 };
-
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -9,9 +9,10 @@ import {
 import { setUser } from "@redux/userSlice";
 import { useAppDispatch } from "@redux/hooks";
 import { LockIcon, ProfileIcon, RightArrowIcon } from "@icons";
-import { Colors, IconSizes } from "@utils/constants";
+import { Colors, DeviceScreen, IconSizes } from "@utils/constants";
 import { InferType, object, string } from "yup";
 import { Formik, FormikErrors } from "formik";
+import { Login } from "@images";
 
 const LoginScreen = () => {
   const dispatch = useAppDispatch();
@@ -56,8 +57,11 @@ const LoginScreen = () => {
   };
 
   return (
-    <View className="flex-1 bg-primaryButton">
-      <View className="mt-[28vh] flex-1 bg-primaryBackground p-4 rounded-t-[50px] pt-[8vh]">
+    <View className="flex-1 bg-secondaryBackground">
+      <View className="absolute top-0 left-0 right-0 bottom-0">
+        <Login width={DeviceScreen.Width} height={DeviceScreen.Height / 2} />
+      </View>
+      <View className="mt-[34vh] flex-1 bg-primaryBackground p-4 rounded-t-[50px] pt-[8vh]">
         <CustomText classNames="text-4xl self-center text-white tracking-[6px] mb-10 font-semibold">
           LOGIN
         </CustomText>
